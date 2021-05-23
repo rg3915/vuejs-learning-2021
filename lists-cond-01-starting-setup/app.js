9e72175426d3fc29fa379e23d0f18f6e02e9b688
Regis Santos <regis42santos@gmail.com>
2021-05-23 03:49:58 -0300

    Rendering Content Conditionally

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -1,6 +1,14 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      enteredGoalValue: '',
+      goals: [],
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+    },
   },
 });
 
