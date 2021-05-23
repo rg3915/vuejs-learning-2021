4d3b09ebbe5ab6c124f6e8bd670931e5ab5582eb
Regis Santos <regis42santos@gmail.com>
2021-05-23 01:43:24 -0300

    Exploring Event Modifiers

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -3,9 +3,16 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: '',
+      confirmedName: '',
     };
   },
   methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert('Submitted!');
+    },
     setName(event, lastName) {
       this.name = event.target.value + ' ' + lastName;
     },
