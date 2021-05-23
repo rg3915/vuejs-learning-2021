860fd7eb487a8cad8fadad1feb54b25b7c3277be
Regis Santos <regis42santos@gmail.com>
2021-05-23 05:11:20 -0300

    Adding a "Game Over" Screen

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      winner: null,
     };
   },
   computed: {
@@ -19,6 +20,26 @@ const app = Vue.createApp({
     },
     mayUseSpecialAttack() {
       return this.currentRound % 3 !== 0;
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // A draw
+        this.winner = 'draw';
+      } else if (value <= 0) {
+        // Player lost
+        this.winner = 'monster';
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        // A draw
+        this.winner = 'draw';
+      } else if (value <= 0) {
+        // Monster lost
+        this.winner = 'player';
+      }
     },
   },
   methods: {
@@ -46,7 +67,7 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healValue;
       }
-      this.attackPlayer()
+      this.attackPlayer();
     },
   },
 });
