52744087b1b95187f7d38af2e1eee98ce63dfa71
Regis Santos <regis42santos@gmail.com>
2021-05-25 08:48:09 -0300

    Vue App Lifecycle

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -12,15 +12,44 @@ const app = Vue.createApp({
     setText() {
       // this.message = this.currentUserInput;
       this.message = this.$refs.userText.value;
+      // console.dir(this.$refs.userText);
     },
+  },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
   },
 });
 
 app.mount('#app');
 
+setTimeout(function () {
+  app.unmount();
+}, 3000);
+
 const app2 = Vue.createApp({
   template: `
-  <p>{{ favoriteMeal }}</p>
+    <p>{{ favoriteMeal }}</p>
   `,
   data() {
     return {
@@ -30,3 +59,25 @@ const app2 = Vue.createApp({
 });
 
 app2.mount('#app2');
+
+// ....
+
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello! World!',
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + ' World!';
+    }
+    target.message = value;
+  },
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'Hello!!!!';
+
+// console.log(proxy.longMessage);
