b4a33fd6616545500794a24e44a620f59cb39cd4
Regis Santos <regis42santos@gmail.com>
2021-05-25 08:02:36 -0300

    One App vs Multiple Apps

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -16,3 +16,13 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+const app2 = Vue.createApp({
+  data() {
+    return {
+      favoriteMeal: 'Pizza',
+    };
+  },
+});
+
+app2.mount('#app2');
