4a994bd5ed0bb650b78c9967bc0e8950c203cb86
Regis Santos <regis42santos@gmail.com>
2021-05-30 18:41:44 -0300

    First Components & Props

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <ul></ul>
+  <ul>
+    <learning-resources
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resources>
+  </ul>
 </template>
 
 <script>
+import LearningResources from './components/learning-resources/LearningResources.vue';
+
 export default {
+  components: {
+    LearningResources
+  },
   data() {
     return {
       storedResources: [
