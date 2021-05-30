9081fd5f5083b014bed052e16706a1de09a648e3
Regis Santos <regis42santos@gmail.com>
2021-05-30 17:10:12 -0300

    Working with Fragments
    Remove div

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,16 +1,14 @@
 <template>
-  <div>
-    <h2>Manage Goals</h2>
-    <input type="text" ref="goal" />
-    <button @click="setGoal">Set Goal</button>
-    <teleport to="body">
-      <error-alert v-if="inputIsInvalid">
-        <h2>Input is invalid!</h2>
-        <p>Please enter at least a few characters...</p>
-        <button @click="confirmError">Okay</button>
-      </error-alert>
-    </teleport>
-  </div>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
+  <teleport to="body">
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please enter at least a few characters...</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
+  </teleport>
 </template>
 
 <script>
