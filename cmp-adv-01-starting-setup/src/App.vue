d06b40fd16dbe4b7ba0b7abf8eeffcd0a7aa75c9
Regis Santos <regis42santos@gmail.com>
2021-05-30 08:09:34 -0300

    Global vs Local Components

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -11,7 +11,16 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
 export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
