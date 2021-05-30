03e4214379234d57715e17cffcee122cfcd4e1c8
Regis Santos <regis42santos@gmail.com>
2021-05-30 08:40:23 -0300

    Slot Styles & Compilation

--- cmp-adv-01-starting-setup/src/components/BaseCard.vue
+++ cmp-adv-01-starting-setup/src/components/BaseCard.vue
@@ -14,6 +14,12 @@ export default {
 </script>
 
 <style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
 div {
   margin: 2rem auto;
   max-width: 30rem;
--- cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -17,19 +17,3 @@ export default {
   props: ['fullName', 'infoText', 'role'],
 };
 </script>
-
-<style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
-section header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-</style>
